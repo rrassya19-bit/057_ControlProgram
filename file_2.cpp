3d6c240eb6c8d466f55ead7a4cfa9d28c3ba4ef3
rrassya19-bit <rrassys19@gmail.com>
2026-03-09 19:05:41 +0700

    menambahkan variable globala di file 2

--- file_2.cpp
+++ file_2.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+float Rata_rata(float a, float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else
+        
+    return "Gagal";
+}
+
+string Status_lulus2(float r){
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
+}
